246c682a15dbfc1b81c84301f6c6a3c8e4a158b1
jumi1234 <farewellsad89@gmail.com>
2021-05-17 15:39:38 +0900

    package 설치 및 기본 라우터 페이지 생성

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <p>Hello Next.js!</p>
+    <Link href="/first-post">first-post 페이지로 가기</Link>
+  </div>
+)
+
+export default Index
